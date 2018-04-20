3c2c4ede387c6df06d77d4e362e622e5f0561a42
klozz <klozz707@gmail.com>
2018-04-19 23:08:32 -0400

    santoni: track xperience doze :y
    Change-Id: Id955e33f133d7d341d791f945130851fefeef030
    Signed-off-by: klozz <klozz707@gmail.com>

--- device.mk
+++ device.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
